e34d71430466ab8dc8652cf73e0a29bef9871b37
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2020-01-11 23:23:15 -0500

    basic colours

--- src/App.js
+++ src/App.js
@@ -6,19 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is a header</h1>
       </header>
+      <body className="App-body">
+        <p>body</p>
+      </body>
     </div>
   );
 }
